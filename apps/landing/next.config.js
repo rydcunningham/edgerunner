062bd7300ecbf1956f895119b50d847a8671858e
rydcunningham <rydcunningham@gmail.com>
2025-01-12 12:52:26 -0600

    hurdur

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: __dirname,
+    outputFileTracingIncludes: {
+      '/**': ['./lib/**/*']
+    }
+  }
 }
 
 module.exports = nextConfig 
