801a2020bc6c6272171e08e31acf193c85b9eee2
rydcunningham <rydcunningham@gmail.com>
2025-01-14 14:42:23 -0800

    homepage mobile view bugs

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Page() {
   return (
     <div className="min-h-screen relative">
-      <div className="grid grid-cols-1 lg:grid-cols-2 min-h-screen">
+      <div className="flex flex-col lg:grid lg:grid-cols-2 min-h-screen">
         {/* Main content */}
-        <div className="flex flex-col items-start justify-center px-12 pl-24 lg:pl-24 lg:pr-0">
+        <div className="flex flex-col items-start justify-center px-12 pl-24 lg:pl-24 lg:pr-0 pt-24 lg:pt-0">
           <div className="space-y-4">
             <div className="flex flex-col items-left lg:items-start">
               <img
@@ -24,8 +24,8 @@ export default function Page() {
         </div>
 
         {/* Terminal text */}
-        <div className="flex items-center justify-start pl-0 lg:pl-12">
-          <div className="font-rajdhani uppercase tracking-widest bg-black/20 p-6 rounded-lg border border-white/10">
+        <div className="flex items-start lg:items-center justify-start pl-24 lg:pl-12 mt-12 lg:mt-0">
+          <div className="font-rajdhani uppercase tracking-widest bg-black/20 p-6 rounded-lg border border-white/10 max-w-[400px]">
             <div className="space-y-3">
               <div><span className="text-white/50">{'>'}</span><span className="text-[#F75049]"> focus: [ai, infra, deeptech]</span></div>
               <div><span className="text-white/50">{'>'}</span><span className="text-[#F75049]"> stage: [angel, pre-seed, seed]</span></div>
